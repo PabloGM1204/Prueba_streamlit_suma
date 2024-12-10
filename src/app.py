39373e3d3128c9d4817777a624260d1986e0641a
PabloGM1204 <pablogm1204@gmail.com>
2024-12-10 11:13:32 +0100

    update app

--- src/app.py
+++ src/app.py
@@ -9,5 +9,5 @@ num_2 = 0
 num_1 = st.number_input("Dime un número")
 num_2 = st.number_input("Dime otro número")
 
-if (num_1 and num_2) == 0.0:
+if (num_1 and num_2) != 0.0:
     st.write(f"La suma de {num_1} y {num_2} es {num_1 + num_2}")
