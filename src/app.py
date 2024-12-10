f958b5322fba556351bb6e159d542d899ecbfd8c
PabloGM1204 <pablogm1204@gmail.com>
2024-12-10 10:51:29 +0100

    first commit

---
+++ src/app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("Suma dos números")
+st.write("Este programa hara que sume dos números")
+
+num_1 = 0
+num_2 = 0
+
+num_1 = st.number_input("Dime un número")
+num_2 = st.number_input("Dime otro número")
+
+if (num_1 and num_2) == 0.0:
+    st.write(f"La suma de {num_1} y {num_2} es {num_1 + num_2}")
